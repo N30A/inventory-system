d13a7d3f8499a55fecfa36619f340df579370e20
= <neo.ahlander@outlook.com>
2025-02-16 18:10:10 +0100

    Added database and env vars

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,28 +1,33 @@
-
 namespace WebApi;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+            .AddEnvironmentVariables();
 
-        // Add services to the container.
-
+        builder.Services.AddDatabase(builder.Configuration, builder.Environment);
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
-
+        
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
+        
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
         }
-
-        app.UseHttpsRedirection();
-
+        else
+        {
+            app.UseHttpsRedirection();
+        }
+        
         app.UseAuthorization();
 
         app.MapControllers();
---
+++ WebApi/ServiceExtensions.cs
@@ -0,0 +1,37 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebApi;
+
+public static class ServiceExtensions
+{
+    private static string GetEnvironmentVariableOrThrow(IConfiguration configuration, string key)
+    {
+        var value = configuration.GetValue<string>(key);
+        if (string.IsNullOrWhiteSpace(value))
+        {   
+            throw new InvalidOperationException($"Environment variable '{key}' is required but was not found.");
+        }
+        return value;
+    }
+    
+    private static string BuildConnectionString(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        string server = GetEnvironmentVariableOrThrow(configuration,"DB_SERVER");
+        string database = GetEnvironmentVariableOrThrow(configuration,"DB_DATABASE");
+        string user = GetEnvironmentVariableOrThrow(configuration,"DB_USER");
+        string password = GetEnvironmentVariableOrThrow(configuration,"DB_PASSWORD");
+        
+        string encrypt = environment.IsDevelopment() ? "False" : "True";
+        string trustCertificate = environment.IsDevelopment() ? "True" : "False";
+
+        return $"Server={server};Database={database};User Id={user};Password={password};Encrypt={encrypt};TrustServerCertificate={trustCertificate};";
+    }
+    
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        string connectionString = BuildConnectionString(configuration, environment);
+        services.AddScoped<IDbConnection>(sc => new SqlConnection(connectionString));
+        return services;
+    }
+}
