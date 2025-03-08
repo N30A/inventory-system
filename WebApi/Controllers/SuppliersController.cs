f0bf8700e424235b10e777b93a567ea87da00b12
= <neo.ahlander@outlook.com>
2025-03-08 19:36:33 +0100

    Implemented GetSuppliers, GetSupplierById & GetSupplierByName endpoints, and more.

--- WebApi/Controllers/SuppliersController.cs
+++ WebApi/Controllers/SuppliersController.cs
@@ -1,5 +1,7 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using Services.Dtos;
+using Services.Interfaces;
 
 namespace WebApi.Controllers;
 
@@ -8,34 +10,67 @@ namespace WebApi.Controllers;
 [ApiVersion("1.0")]
 public class SuppliersController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult GetSuppliers()
+    private readonly ISupplierService _supplierService;
+    
+    public SuppliersController(ISupplierService supplierService)
     {
-        throw new NotImplementedException();
+        _supplierService = supplierService;
     }
     
-    [HttpGet("{supplierId:int}")]
-    public IActionResult GetSupplierById(int supplierId)
+    [HttpGet]
+    public async Task<IActionResult> GetSuppliers()
     {
-        throw new NotImplementedException();
+        var result = await _supplierService.GetAllAsync();
+        var data = result.Data ?? new List<SupplierDto>();
+        
+        if (!result.Status)
+        {
+            return NotFound(new Response<IEnumerable<SupplierDto>>(data, result.Message));
+        }
+        
+        return Ok(new Response<IEnumerable<SupplierDto>>(data));
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetSupplierById(int id)
+    {
+        var result = await _supplierService.GetByIdAsync(id);
+        if (!result.Status)
+        {
+            return NotFound(new Response<SupplierDto?>(result.Data, result.Message));
+        }
+        
+        return Ok(new Response<SupplierDto?>(result.Data));
+    }
+    
+    [HttpGet("{name}")]
+    public async Task<IActionResult> GetSupplierByName(string name)
+    {
+        var result = await _supplierService.GetByNameAsync(name);
+        if (!result.Status)
+        {
+            return NotFound(new Response<SupplierDto?>(result.Data, result.Message));
+        }
+        
+        return Ok(new Response<SupplierDto?>(result.Data));
     }
     
     // TODO: object -> correct type
     [HttpPost]
-    public IActionResult CreateSupplier([FromBody] object body) 
+    public async Task<IActionResult> CreateSupplier([FromBody] SupplierDto body) 
     {
         throw new NotImplementedException();
     }
     
     // TODO: object -> correct type
-    [HttpPut("{supplierId:int}")]
-    public IActionResult UpdateSupplier(int supplierId, [FromBody] object body)
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateSupplier(int id, [FromBody] SupplierDto body)
     {
         throw new NotImplementedException();
     }
     
-    [HttpDelete("{supplierId:int}")]
-    public IActionResult DeleteSupplierById(int supplierId)
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteSupplierById(int id)
     {
         throw new NotImplementedException();
     }
