3c6f6f1688467892b976177f5c4dfbfe1f8942c6
= <neo.ahlander@outlook.com>
2025-03-12 19:10:36 +0100

    Code refactor and CreateSupplier implementation

--- WebApi/Controllers/SuppliersController.cs
+++ WebApi/Controllers/SuppliersController.cs
@@ -8,6 +8,7 @@ namespace WebApi.Controllers;
 [ApiController]
 [Route("/api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
+[Produces("application/json")]
 public class SuppliersController : ControllerBase
 {
     private readonly ISupplierService _supplierService;
@@ -25,10 +26,14 @@ public class SuppliersController : ControllerBase
         
         if (!result.Status)
         {
-            return NotFound(new Response<IEnumerable<SupplierDto>>(data, result.Message));
+            return NotFound(new Response<IEnumerable<SupplierDto>>
+            {
+                Data = data,
+                Message = result.Message
+            });
         }
         
-        return Ok(new Response<IEnumerable<SupplierDto>>(data));
+        return Ok(new Response<IEnumerable<SupplierDto>> { Data = data });
     }
     
     [HttpGet("{id:int}")]
@@ -37,10 +42,14 @@ public class SuppliersController : ControllerBase
         var result = await _supplierService.GetByIdAsync(id);
         if (!result.Status)
         {
-            return NotFound(new Response<SupplierDto?>(result.Data, result.Message));
+            return NotFound(new Response<SupplierDto?>
+            {
+                Data = result.Data,
+                Message = result.Message
+            });
         }
         
-        return Ok(new Response<SupplierDto?>(result.Data));
+        return Ok(new Response<SupplierDto?> { Data = result.Data });
     }
     
     [HttpGet("{name}")]
@@ -49,17 +58,51 @@ public class SuppliersController : ControllerBase
         var result = await _supplierService.GetByNameAsync(name);
         if (!result.Status)
         {
-            return NotFound(new Response<SupplierDto?>(result.Data, result.Message));
+            return NotFound(new Response<SupplierDto?>
+            {
+                Data = result.Data,
+                Message = result.Message
+            });
         }
         
-        return Ok(new Response<SupplierDto?>(result.Data));
+        return Ok(new Response<SupplierDto?> { Data = result.Data } );
     }
     
-    // TODO: object -> correct type
     [HttpPost]
-    public async Task<IActionResult> CreateSupplier([FromBody] SupplierDto body) 
+    public async Task<IActionResult> CreateSupplier([FromBody] CreateSupplierDto body) 
     {
-        throw new NotImplementedException();
+        if (!ModelState.IsValid)
+        {   
+            string message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+            
+            return BadRequest(new Response<CreateSupplierDto>
+            {
+                Data = body,
+                Message = message
+            });
+        }
+        
+        var result = await _supplierService.AddAsync(body);
+        if (!result.Status)
+        {
+            return BadRequest(new Response<CreateSupplierDto>
+            {
+                Data = body,
+                Message = result.Message
+            });
+        }
+        
+        return CreatedAtAction(nameof(GetSupplierById), new { id = result.Data }, new Response<SupplierDto>
+        {
+            Data = new SupplierDto
+            {
+                Id = (int)result.Data!,
+                Name = body.Name,
+                Address = body.Address,
+                Phone = body.Phone,
+                Email = body.Email
+            }
+        });
     }
     
     // TODO: object -> correct type
